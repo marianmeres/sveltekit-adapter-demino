1f215355bc147d74332248ba179901a25b04b1a3
Marian Meres <marian@meres.sk>
2026-04-29 08:34:34 +0200

    docs

--- src/mod.ts
+++ src/mod.ts
@@ -1,8 +1,6 @@
 /**
  * SvelteKit adapter for Deno — generates a `handler.js` compatible with
  * {@link https://jsr.io/@marianmeres/demino demino} and any `Deno.ServeHandler` server.
- *
- * @module
  */
 export { default } from "./sveltekit-adapter-demino.ts";
 export * from "./sveltekit-adapter-demino.ts";
